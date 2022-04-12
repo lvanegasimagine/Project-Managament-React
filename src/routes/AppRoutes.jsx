0c8af3822aa2930d5ae171cf76665357da4e34e5
Luis Vanegas <lvanegas1429@gmail.com>
2022-04-12 09:40:46 -0600

    React-Router-Dom instalado y Creacion de AppRoutes con sus rutas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Create from "../pages/create/Create";
+import Dashboard from "../pages/dashboard/Dashboard";
+import Login from "../pages/login/Login";
+import Project from "../pages/project/Project";
+import Signup from "../pages/signup/Signup";
 
 const AppRoutes = () => {
   return (
-    <div>AppRoutes</div>
-  )
-}
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+          <Route path='/create'>
+            <Create/>
+          </Route>
+          <Route path='/projects/:id'>
+            <Project/>
+          </Route>
+          <Route path='/login'>
+            <Login/>
+          </Route>
+          <Route path='/signup'>
+            <Signup/>
+          </Route>
+        </Switch>
+      </Router>
+    </Fragment>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
