3d26a9eaa34a6ddc8bd1874b35076e617fb805b5
Luis Vanegas <lvanegas1429@gmail.com>
2022-04-12 18:54:17 -0600

    Signup maquetacion creada

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,9 +1,33 @@
 import './Signup.css';
 
-const Signup = () => {
+import React, { useState } from 'react'
+
+export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [thumbnail, setThumbnail] = useState(null);
+
   return (
-    <div>Signup</div>
+    <form className='auth-form'>
+      <h2>Sign Up</h2>
+      <label htmlFor="email">
+        <span>Email:</span>
+        <input id='email' type="email" required onChange={(e) => setEmail(e.target.value)} value={email} />
+      </label>
+      <label htmlFor="password">
+        <span>Password:</span>
+        <input id='password' type="password" required onChange={(e) => setPassword(e.target.value)} value={password} />
+      </label>
+      <label htmlFor="displayName">
+        <span>Display Name:</span>
+        <input id='displayName' type="text" required onChange={(e) => setDisplayName(e.target.value)} value={displayName} />
+      </label>
+      <label htmlFor="profile">
+        <span>Profile Thumbnail: </span>
+        <input id='profile' type="file" required />
+      </label>
+      <button className="btn">Sign Up</button>
+    </form>
   )
 }
-
-export default Signup
