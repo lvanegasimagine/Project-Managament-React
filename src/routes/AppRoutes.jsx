f590b70e94a927f80e73a52310876d737e136086
Luis Vanegas <lvanegas1429@gmail.com>
2022-04-12 18:21:32 -0600

    Navbar Implementado

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
 import Create from "../pages/create/Create";
 import Dashboard from "../pages/dashboard/Dashboard";
 import Login from "../pages/login/Login";
@@ -8,27 +9,28 @@ import Signup from "../pages/signup/Signup";
 
 const AppRoutes = () => {
   return (
-    <Fragment>
-      <Router>
+    <Router>
+      <div className="container">
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Dashboard />
           </Route>
-          <Route path='/create'>
-            <Create/>
+          <Route path="/create">
+            <Create />
           </Route>
-          <Route path='/projects/:id'>
-            <Project/>
+          <Route path="/projects/:id">
+            <Project />
           </Route>
-          <Route path='/login'>
-            <Login/>
+          <Route path="/login">
+            <Login />
           </Route>
-          <Route path='/signup'>
-            <Signup/>
+          <Route path="/signup">
+            <Signup />
           </Route>
         </Switch>
-      </Router>
-    </Fragment>
+      </div>
+    </Router>
   );
 };
 
