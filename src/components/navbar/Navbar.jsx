79a2c78d9d690e23cde20887ff6b7ecad2b4ec37
Luis Vanegas <lvanegas1429@gmail.com>
2022-04-14 15:02:43 -0600

    Condicional NavBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,48 @@
-import './Navbar.css'
-import Temple from '../../assets/temple.svg';
-import { Link } from 'react-router-dom';
-import { useLogout } from '../../hooks/useLogout';
+import "./Navbar.css";
+import Temple from "../../assets/temple.svg";
+import { Link } from "react-router-dom";
+import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 const Navbar = () => {
-
-  const {logout, isPending } = useLogout();
+  const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
   return (
-    <div className='navbar'>
-        <ul>
-            <li className='logo'>
-                <img src={Temple} alt="Dojo Logo" />
-                <span>The Dojo</span>
-            </li>
-            <li><Link to='/login'>Login</Link> </li>
-            <li><Link to='/signup'>Signup</Link> </li>
+    <div className="navbar">
+      <ul>
+        <li className="logo">
+          <img src={Temple} alt="Dojo Logo" />
+          <span>The Dojo</span>
+        </li>
+        {!user && (
+          <>
             <li>
-                {!isPending && <button className='btn' onClick={logout}>Logout</button>}
-                {isPending && <button className='btn' disabled>Logging out...</button>}
+              <Link to="/login">Login</Link>
             </li>
-        </ul>
+            <li>
+              {" "}
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            {!isPending && (
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            )}
+            {isPending && (
+              <button className="btn" disabled>
+                Logging out...
+              </button>
+            )}
+          </li>
+        )}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
