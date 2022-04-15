ca6512e290341207d9739cec5484f5210d3a761b
Luis Vanegas <lvanegas1429@gmail.com>
2022-04-15 16:46:49 -0600

    Eliminando Proyecto por el creado de dicho proyecto

--- src/pages/project/ProjectSummary/ProjectSummary.jsx
+++ src/pages/project/ProjectSummary/ProjectSummary.jsx
@@ -1,11 +1,24 @@
 import "./ProjectSummary.css";
 import Avatar from "../../../components/avatar/Avatar";
+import { useFirestore } from "../../../hooks/useFirestore";
+import { useAuthContext } from "../../../hooks/useAuthContext";
+import { useHistory } from "react-router-dom";
 
 export default function ProjectSummary({ project }) {
+
+  const history = useHistory();
+  const { user } = useAuthContext();
+  const { deleteDocument } = useFirestore("projects");
+
+  const handleDelete = (e) => {
+    deleteDocument(project.id);
+    history.push("/");
+  };
   return (
     <div>
       <div className="project-summary">
         <h2 className="page-title">{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
         <p className="due-date">
           Project due By {project.dueDate.toDate().toDateString()}
         </p>
@@ -18,6 +31,11 @@ export default function ProjectSummary({ project }) {
             </div>
           ))}
         </div>
+        {user.uid === project.createdBy.id && (
+          <button className="btn" onClick={handleDelete}>
+            Mark as Complete
+          </button>
+        )}
       </div>
     </div>
   );
