c07c2b241485ed5728412cf5c6d4f0c9841a1cdb
Luis Vanegas <lvanegas1429@gmail.com>
2022-04-15 16:39:37 -0600

    Listando los comentarios

--- src/pages/project/ProjectComments/ProjectComment.jsx
+++ src/pages/project/ProjectComments/ProjectComment.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Avatar from "../../../components/avatar/Avatar"
 import { timestamp } from "../../../firebase/config"
 import { useAuthContext } from "../../../hooks/useAuthContext"
 import { useFirestore } from "../../../hooks/useFirestore"
@@ -34,6 +35,22 @@ export default function ProjectComment({project}) {
     <div className="project-comments">
       <h4>Project Comments</h4>
 
+      <ul>
+        {project.comments.length > 0 && project.comments.map(comment =>(
+          <li key={comment.id}>
+            <div className="comment-author">
+              <Avatar src={comment.photoURL}/>
+              <p>{comment.displayName}</p>
+            </div>
+            <div className="comment-date">
+              <p>Date Here</p>
+            </div>
+            <div className="comment-content">
+              <p>{comment.content}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
       <form className="add-comment" onSubmit={handleSubmit}>
         <label htmlFor="comment">
           <span>Add New Comment:</span>
